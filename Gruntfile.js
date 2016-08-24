4d1dc50471b04a5a2c659542357c6d4e0a848b87
RomanVytak <roman.vytak@gmail.com>
2016-08-24 14:43:59 +0300

    first

---
+++ Gruntfile.js
@@ -0,0 +1,234 @@
+module.exports = function (grunt) {
+    var http = require('http');
+    var send = require('send');
+    var url = require('url');
+    var fs = require('fs');
+    //var localAssets = 'pictures';
+    var localAssets2 = 'img';
+    // Project configuratasdion.
+    grunt.registerTask('prepareJSON', 'Preapring JSON', function() {
+        var data = String(fs.readFileSync('app/templates/data/index.json'));
+        var replaced = {
+            local:data.replace(/~/gm,localAssets2)
+        }
+        for(var type in replaced){
+            fs.writeFileSync("temp/index_"+type+".json",replaced[type]);    
+        }
+    });
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        connect: {
+            server: {
+                options: {
+                    port: 9001,
+                    listen: '192.168.0.15',
+                    base: '',
+                    debug: true,
+                    middleware: function (connect, options) {
+                        // Return array of whatever middlewares you want
+                        return [
+                            //connect.static(options.base),
+                            function (req, res, next) {
+
+                                function error(err) {
+                                    res.statusCode = err.status || 500;
+                                    res.end('Error: ' + err.message);
+                                }
+
+                                // your custom directory handling logic:
+                                function redirect() {
+                                    res.statusCode = 301;
+                                    res.setHeader('Location', req.url + '/');
+                                    res.end('Redirecting to ' + req.url + '/');
+                                }
+
+                                // transfer arbitrary files from within
+                                // /www/example.com/public/*
+                                var path = url.parse(req.url).pathname;
+
+                                if (path.indexOf('bower_components') > -1) {
+                                    console.log(path);
+                                    var appPath = '';
+                                } else {
+                                    var appPath = '/app';
+                                }
+
+                                send(req, path)
+                                    .root(__dirname + appPath)
+                                    .on('error', error)
+                                    .index('index.html')
+                                    .hidden(true)
+                                    .on('directory', redirect)
+                                    .pipe(res);
+                            }
+                            ];
+                    }
+                }
+            }
+        },
+        uglify: {
+            my_target: {
+                files: {
+                    'app/js/build/index.min.js':
+                    [
+                        // 'app/js/vendor/helpers.js',
+                        'bower_components/jquery/dist/jquery.min.js',
+                        'bower_components/mustache/mustache.js',
+                        'bower_components/masonry/dist/masonry.pkgd.min.js',
+                        'bower_components/scrollmagic/scrollmagic/minified/ScrollMagic.min.js',
+                        'bower_components/scrollmagic/scrollmagic/minified/plugins/debug.addIndicators.min.js',
+                        'app/js/vendor/hamster.js',
+                        'app/js/app/app.js'
+                    ]
+                }
+            }
+        },
+        // якщо треба виключити автоматичне заливання проекту на фтп потрібно
+        // просто затерти 'ftp-deploy' задачу як показано нижче
+        // tasks: ['mustache_render', 'ftp-deploy'] має бути tasks: ['mustache_render']
+        watch: {
+            templates: {
+                files: ['app/templates/**/*.html', 'temp/**/*.json'],
+                tasks: ['mustache_render']
+            },
+            sass: {
+                files: ['scss/**/*.scss'],
+                tasks: ['sass:dist']
+            },
+            css: {
+                files: ['temp/app.css'],
+                tasks: ['autoprefixer']
+            },
+            js: {
+                files: ['app/js/app/**/*.js'],
+                tasks: ['uglify']
+            },
+            json:{
+               files: ['app/templates/**/*.json'],
+               tasks: ['prepareJSON']
+            }
+        },
+        
+        'ftp-deploy': {
+            build: {
+                auth: {
+                    host: 'bambuky.ftp.ukraine.com.ua',
+                    port: 21,
+                    authPath: '.ftppass',
+                    authKey: 'key1'
+                },
+                src: 'app',
+                dest: 'bambus.com.ua/tests/termobud',
+                exclusions: ['app/templates']
+            }
+        },
+
+        mustache_render: {
+            options: {
+                directory: 'app/templates/partials',
+                extension: '.html',
+                prefix: ''
+            },
+            all: { 
+                files: [
+                    {
+                        data: 'app/templates/data/index.json',
+                        template: 'app/templates/_index.html',
+                        dest: 'app/index.html'
+                    },
+                    {
+                        data: 'app/templates/data/index.json',
+                        template: 'app/templates/_blog.html',
+                        dest: 'app/blog.html'
+                    },
+                    {
+                        data: 'app/templates/data/index.json',
+                        template: 'app/templates/_news.html',
+                        dest: 'app/news.html'
+                    },
+                    {
+                        data: 'app/templates/data/index.json',
+                        template: 'app/templates/_about.html',
+                        dest: 'app/about.html'
+                    },
+                    {
+                        data: 'app/templates/data/index.json',
+                        template: 'app/templates/_portfolio.html',
+                        dest: 'app/portfolio.html'
+                    },
+                    {
+                        data: 'app/templates/data/index.json',
+                        template: 'app/templates/_montage.html',
+                        dest: 'app/montage.html'
+                    }
+          ]
+            },
+        },
+        sass: {
+            dist: {
+                options: {
+                    //outputStyle: 'compressed',
+                    includePaths: [
+                        'bower_components/foundation/scss/'
+                    ]
+                },
+                files: {
+                    'temp/app.css': 'scss/app.scss',
+                }
+            }
+        },
+        autoprefixer: {
+            options: {
+                browsers: ['last 2 version', 'ie 9', 'Safari 5']
+            },
+            multiple_files: {
+                  expand: true,
+                  flatten: true,
+                  src: 'temp/**/*.css', // -> src/css/file1.css, src/css/file2.css
+                  dest: 'app/css/' // -> dest/css/file1.css, dest/css/file2.css
+            },
+
+        },
+        browser_sync: {
+            files: {
+                src: [
+                    'app/index.html',
+                    'app/js/**/*.js',
+                    'app/css/app.css'
+                ]
+            },
+            options: {
+                host: 'localhost',
+                reloadFileTypes: ['php', 'html', 'js', 'erb', 'svg'],
+                injectFileTypes: ['css', 'png', 'jpg', 'svg', 'gif'],
+                watchTask: true,
+                debugInfo: true,
+                ghostMode: {
+                    scroll: true,
+                    forms: true
+                }
+
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-mustache-render');
+    grunt.loadNpmTasks('grunt-browser-sync');
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-ftp-deploy');
+
+    grunt.event.on('watch', function (action, filepath, target) {
+        grunt.config.set('myFile', filepath);
+        grunt.config.set('_myFile', filepath.replace(/\\/g, '/'));
+        console.log(filepath.replace(/\\/g, '/'))
+    });
+
+    // Default task(s).
+    grunt.registerTask('default', ['connect', 'browser_sync', 'sass', 'watch']);
+
+};
---
+++ app/js/app/app.js
@@ -0,0 +1,411 @@
+(function() {
+
+	// var -------------------------------------------------------------------------------------------- //
+	var _body = $('body');
+
+
+
+	// start! ----------------------------------------------------------------------------------------- //
+	window.addEventListener && window.addEventListener('DOMContentLoaded', function(){
+		document.body.className += ' dom-loaded';
+
+		$('.blog .items-wrapper').masonry({
+			columnWidth: '.item'
+		});
+
+		// my functions
+		openMenu();
+		if(_body.hasClass('home-page')) {
+			scrollControll_home();
+			scrollControll_projects();
+			scrollControll_contacts();
+			checkPosition();
+		}
+
+		$.fn.disableScroll = function() {
+			window.oldScrollPos = $(window).scrollTop();
+			$(window).on('scroll.scrolldisabler',function ( event ) {
+				$(window).scrollTop( window.oldScrollPos );
+				event.preventDefault();
+			});
+		};
+		$.fn.enableScroll = function() {
+			$(window).off('scroll.scrolldisabler');
+		};
+	});
+
+	// show site -------------------------------------------------------------------------------------- //
+	window.addEventListener && window.addEventListener('load', function(){
+		document.body.className += ' loaded';
+		$('.step-3 .video-wrapper').addClass('show-video');
+
+		// my functions
+		magellan();
+		sliderHome();
+		
+		// home page
+		var videoHome1 = document.getElementById('video-home-1');
+		var videoHome2 = document.getElementById('video-home-2');
+		var videoHome3 = document.getElementById('video-home-3');
+		videoHome1.play();
+		videoHome2.play();
+		videoHome3.play();
+
+	});
+
+
+
+	// open menu
+	function openMenu() {
+		$('.menu-button').on('click', function() {
+			_body.toggleClass('open-menu');
+		})
+	}
+
+
+
+	// magellan
+	function magellan() {
+		var heightStep1 = $('.step-1').height(),
+			magellanBlock = $('.magellan');
+		$(window).scroll(function() {
+			var scrollTop = $(window).scrollTop();
+			if(scrollTop >= heightStep1) {
+				magellanBlock.addClass('show');
+			} else {
+				magellanBlock.removeClass('show');
+			}
+		});
+	}
+
+
+
+	// slider (home page)
+	function sliderHome() {
+		var controls = $('.step-3 .slider-wrapper .controls > div > div > div'),
+			num = 1,
+			wrapper = $('.step-3 .slider-wrapper .wrapper'),
+			numberProjects = $('.step-3 .slider-wrapper .wrapper > div').length;
+		controls.on('click', function() {
+			if($(this).hasClass('prev')) {
+				if(num > 1) {
+					num = num - 1
+				} else {
+					num = numberProjects
+				}
+			}
+			if($(this).hasClass('next')) {
+				if(num < numberProjects) {
+					num = num + 1
+				} else {
+					num = 1
+				}
+			}
+			wrapper.attr('project', 'project-' + num);
+		})
+	}
+
+
+
+	function scrollControll_home() {
+		var controller = new ScrollMagic.Controller();
+
+		var scene = new ScrollMagic.Scene({
+			triggerElement: '.step-1'
+		})
+		.addIndicators({
+			name: 'home'
+		})
+		.addTo(controller)
+		scene.on('start', function (event) {
+			$('.step-1').addClass('active');
+		});
+	}
+
+
+
+	// scroll controll (projects)
+	var position = $('.step-3'),
+		stopPosition = $('.step-3').offset().top,
+		lastScrollTop = 0,
+		duration = 600 + 100;
+	var wheel = null;
+	function checkPosition() {
+		if($(window).scrollTop() <= stopPosition-10) {
+			position.attr('position', 'position-1');
+		} else {
+			position.attr('position', 'position-3');
+			position.attr('step', 'content-3');
+		}
+	}
+	function scrollControll_projects() {
+		var step3 = $('.step-3');
+
+		var controller = new ScrollMagic.Controller({
+			globalSceneOptions: {
+				triggerHook: 'onLeave'
+			}
+		});
+
+		var scene = new ScrollMagic.Scene({
+			triggerElement: '.step-3',
+			duration: 200
+		})
+		.setPin('.step-3')
+		.addIndicators({
+			name: 'first'
+		})
+		.addTo(controller)
+
+		scene.on('enter', function (event) {
+			$(window).disableScroll();
+			function handler(event, delta, deltaX, deltaY) {
+				if (deltaY === -1) {
+					if (position.attr('position') == 'position-1') {
+						// change position
+						position.attr('position', '');
+						setTimeout(function() {
+							position.attr('position', 'position-2');
+						}, duration);
+						// change content
+						position.attr('step', 'content-2');
+					}
+					if (position.attr('position') == 'position-2') {
+						// change position
+						position.attr('position', '');
+						setTimeout(function() {
+							position.attr('position', 'position-3');
+						}, duration);
+						// change content
+						position.attr('step', 'content-3');
+					}
+					if (position.attr('position') == 'position-3') {
+						wheel.unwheel();
+						$(window).enableScroll();
+					}
+				}
+				if(deltaY === 1) {
+					if (position.attr('position') == 'position-3') {
+						// change position
+						position.attr('position', '');
+						setTimeout(function() {
+							position.attr('position', 'position-2');
+						}, duration);
+						// change content
+						position.attr('step', 'content-2');
+					}
+					if (position.attr('position') == 'position-2') {
+						// change position
+						position.attr('position', '');
+						setTimeout(function() {
+							position.attr('position', 'position-1');
+						}, duration);
+						// change content
+						position.attr('step', '');
+					}
+					if (position.attr('position') == 'position-1') {
+						wheel.unwheel();
+						$(window).enableScroll();
+					}
+				}
+				event.preventDefault();
+			}
+			wheel = Hamster(window).wheel(handler, false);
+		});
+		scene.on('leave', function(event) {
+			console.log("Scene leaved.");
+			$(window).enableScroll();
+			wheel.unwheel();
+		});
+	}
+
+
+
+	function scrollControll_contacts() {
+		var controller = new ScrollMagic.Controller();
+
+		var scene = new ScrollMagic.Scene({
+			triggerElement: '.step-4'
+		})
+		// .addIndicators({
+		// 	name: 'contacts'
+		// })
+		.addTo(controller)
+		scene.on('start', function (event) {
+			$('.step-4').addClass('active');
+		});
+	}
+
+
+
+
+
+
+
+
+
+
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	// function scrollControll() {
+
+	// 	var stopPosition = $('.step-3').offset().top,
+	// 		step3 = $('.step-3'),
+	// 		scrollTop = $(window).scrollTop(),
+	// 		lastScrollTop = 0,
+	// 		position = $('.step-3'),
+	// 		duration = 700 + 200;
+
+	// 	// показати відео (для анімації)
+	// 	$('.step-3 .video-wrapper').addClass('show-video');
+
+	// 	// запустити функцію при оновленні сторінки
+	// 	checkPosition();
+
+	// 	// при скролі
+	// 	$(window).scroll(function(event){
+	// 		var st = $(this).scrollTop();
+	// 		if (st > lastScrollTop){
+	// 			console.log('down');
+	// 			scrollTop = $(window).scrollTop();
+	// 			checkPosition();
+	// 		} else {
+	// 			console.log('up');
+	// 		}
+	// 		lastScrollTop = st;
+	// 	});
+
+	// 	// чек позиції
+	// 	function checkPosition() {
+	// 		if(scrollTop >= stopPosition-10) {
+	// 			// 
+	// 			position.attr('position', 'position-1');
+	// 			// блокувати скрол
+	// 			position.addClass('disable-scroll');
+	// 			// 
+	// 			changeContent();
+	// 		}
+	// 	}
+
+	// 	// 
+	// 	function changeContent() {
+	// 		var disableScroll = document.querySelector('.disable-scroll'),
+	// 			index = 0;
+
+	// 		function handler(event, delta, deltaX, deltaY) {
+	// 			//console.log(event);
+
+	// 			if (step3.hasClass('disable-scroll')) {
+	// 				event.preventDefault();
+
+	// 				// go down!
+	// 				if(delta == '-1') {
+	// 					if (position.attr('position') == 'position-1') {
+	// 						// change position
+	// 						position.attr('position', '');
+	// 						setTimeout(function() {
+	// 							position.attr('position', 'position-2');
+	// 						}, duration);
+	// 						// change content
+	// 						position.attr('step', 'content-2');
+	// 					}
+	// 					if (position.attr('position') == 'position-2') {
+	// 						// change position
+	// 						position.attr('position', '');
+	// 						setTimeout(function() {
+	// 							position.attr('position', 'position-3');
+	// 						}, duration);
+	// 						// change content
+	// 						position.attr('step', 'content-3');
+	// 					}
+	// 					if (position.attr('position') == 'position-3') {
+	// 						step3.removeClass('disable-scroll')
+	// 					}
+	// 				}
+
+	// 				// go up!
+	// 				if(delta == '1') {
+	// 					if (position.attr('position') == 'position-3') {
+	// 						// change position
+	// 						position.attr('position', '');
+	// 						setTimeout(function() {
+	// 							position.attr('position', 'position-2');
+	// 						}, duration);
+	// 						// change content
+	// 						position.attr('step', 'content-2');
+	// 					}
+	// 					if (position.attr('position') == 'position-2') {
+	// 						// change position
+	// 						position.attr('position', '');
+	// 						setTimeout(function() {
+	// 							position.attr('position', 'position-1');
+	// 						}, duration);
+	// 						// change content
+	// 						position.attr('step', '');
+	// 					}
+	// 					if (position.attr('position') == 'position-1') {
+	// 						step3.removeClass('disable-scroll')
+	// 					}
+	// 				}
+	// 			}
+	// 		}
+	// 		Hamster(disableScroll).wheel(handler,false);
+	// 	}
+	// }
