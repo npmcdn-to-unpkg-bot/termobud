847813b8c6ba32d452d331256903c00cd5b53398
RomanVytak <roman.vytak@gmail.com>
2016-08-25 19:28:39 +0300

    new

--- app/js/app/app.js
+++ app/js/app/app.js
@@ -15,6 +15,7 @@
 
 		// my functions
 		openMenu();
+		sliderReviews();
 		if(_body.hasClass('home-page')) {
 			scrollControll_home();
 			scrollControll_projects();
@@ -82,26 +83,122 @@
 
 	// slider (home page)
 	function sliderHome() {
-		var controls = $('.step-3 .slider-wrapper .controls > div > div > div'),
-			num = 1,
-			wrapper = $('.step-3 .slider-wrapper .wrapper'),
-			numberProjects = $('.step-3 .slider-wrapper .wrapper > div').length;
-		controls.on('click', function() {
-			if($(this).hasClass('prev')) {
-				if(num > 1) {
-					num = num - 1
-				} else {
-					num = numberProjects
+		var controll = $('.home-page .slider-wrapper .controls .controll'),
+			lengthElem = $('.home-page .slider-wrapper .items .item').length,
+			reviewsWrapper = $('.home-page .slider-wrapper .items');
+
+		controll.on('click', function() {
+
+			if(!reviewsWrapper.hasClass('waiting')) {
+				var activeElement = $('.home-page .slider-wrapper .items .item.active'),
+					indexElem = $('.home-page .slider-wrapper .items .item.active').index();
+
+				// waiting
+				reviewsWrapper.addClass('waiting');
+				setTimeout(function() {
+					reviewsWrapper.removeClass('waiting');
+				}, 600*2)
+
+				// prev
+				if($(this).hasClass('prev')) {
+					$('.home-page .slider-wrapper .items .item').removeClass('active');
+					setTimeout(function() {
+						// check position
+						if(indexElem >= 1) { // якщо не перший елемент
+							activeElement.prev('.item').addClass('active');
+						} else { // якщо перший
+							$('.home-page .slider-wrapper .items .item:last-child').addClass('active');
+						}
+					}, 600)
+				}
+				// next
+				else {
+					$('.home-page .slider-wrapper .items .item').removeClass('active');
+					setTimeout(function() {
+						// ckeck position
+						if(indexElem < lengthElem-1) { // якщо не останній елемент
+							activeElement.next('.item').addClass('active');
+						} else { // якщо останній елемент
+							$('.home-page .slider-wrapper .items .item:first-child').addClass('active');
+						}
+					}, 600)
 				}
 			}
-			if($(this).hasClass('next')) {
-				if(num < numberProjects) {
-					num = num + 1
-				} else {
-					num = 1
+		})
+
+
+
+
+
+
+		// var controls = $('.step-3 .slider-wrapper .controls > div > div > div'),
+		// 	num = 1,
+		// 	wrapper = $('.step-3 .slider-wrapper .wrapper'),
+		// 	numberProjects = $('.step-3 .slider-wrapper .wrapper > div').length;
+		// controls.on('click', function() {
+		// 	if($(this).hasClass('prev')) {
+		// 		if(num > 1) {
+		// 			num = num - 1
+		// 		} else {
+		// 			num = numberProjects
+		// 		}
+		// 	}
+		// 	if($(this).hasClass('next')) {
+		// 		if(num < numberProjects) {
+		// 			num = num + 1
+		// 		} else {
+		// 			num = 1
+		// 		}
+		// 	}
+		// 	wrapper.attr('project', 'project-' + num);
+		// })
+	}
+
+
+
+	// slider reviews
+	function sliderReviews() {
+		var controll = $('.about .reviews-wrapper .controls .controll'),
+			lengthElem = $('.about .reviews-wrapper .item').length,
+			reviewsWrapper = $('.about .reviews-wrapper .items');
+
+		controll.on('click', function() {
+
+			if(!reviewsWrapper.hasClass('waiting')) {
+				var activeElement = $('.about .reviews-wrapper .item.active'),
+					indexElem = $('.about .reviews-wrapper .item.active').index();
+
+				// waiting
+				reviewsWrapper.addClass('waiting');
+				setTimeout(function() {
+					reviewsWrapper.removeClass('waiting');
+				}, 600*2)
+
+				// prev
+				if($(this).hasClass('prev')) {
+					$('.about .reviews-wrapper .item').removeClass('active');
+					setTimeout(function() {
+						// check position
+						if(indexElem >= 1) { // якщо не перший елемент
+							activeElement.prev('.item').addClass('active');
+						} else { // якщо перший
+							$('.about .reviews-wrapper .item:last-child').addClass('active');
+						}
+					}, 600)
+				}
+				// next
+				else {
+					$('.about .reviews-wrapper .item').removeClass('active');
+					setTimeout(function() {
+						// ckeck position
+						if(indexElem < lengthElem-1) { // якщо не останній елемент
+							activeElement.next('.item').addClass('active');
+						} else { // якщо останній елемент
+							$('.about .reviews-wrapper .item:first-child').addClass('active');
+						}
+					}, 600)
 				}
 			}
-			wrapper.attr('project', 'project-' + num);
 		})
 	}
 
